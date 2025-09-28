ae8ed2ab1d14553425f2f5c7b239d1243568b1f2
kekeIRL <144775386+kekeIRL@users.noreply.github.com>
2025-09-28 14:28:48 +0300

    Add files via upload

---
+++ practice4/ConsoleApplicationINFO.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    float x, y;
+    std::cout << "input x: ";  std::cin >> x;
+    std::cout << "input y: ";  std::cin >> y;
+
+    float R = 1 / sin(y*y/x);
+    float S = exp(1 / x) * log(y);
+    float c = std::max(R, S);
+    
+    std::cout << R << ", " << S << '\n' << c;
+    std::cin.get();
+    std::cin.get();
+    return 0;
+}
